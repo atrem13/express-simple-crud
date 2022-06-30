fbe405a050cc11fdb3d918499f0b0a6b1925ead9
atrem13 <mertayoga17.my@gmail.com>
2022-06-30 14:51:29 +0800

    init

---
+++ models/productModel.js
@@ -0,0 +1,17 @@
+import {DataTypes, Sequelize} from 'sequelize';
+import db from '../config/database';
+
+const {DataTypes} = Sequelize;
+
+const Product = db.define('products', {
+    title: {
+        type: DataTypes.STRING
+    },
+    price: {
+        type: DataTypes.DOUBLE
+    }
+},{
+    freezeTableName: true
+});
+
+export default Product;
